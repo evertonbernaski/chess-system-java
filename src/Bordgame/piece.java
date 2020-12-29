fd586af45ec7de15560a9943263da20a8a3d2ec8
evertonbernaski <admtradereverton@gmail.com>
2020-12-29 19:42:41 -0300

    Starting to implement Board and Piece

---
+++ src/Bordgame/piece.java
@@ -0,0 +1,17 @@
+package Bordgame;
+
+public class piece {
+
+	protected Position position;
+	private Board board;
+	public piece(Board board) {
+		
+		this.board = board;
+		position = null; 
+		
+	}
+	protected Board getBoard() {
+		return board;
+	}
+	
+}
